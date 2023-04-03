e63c5299f50955468a79d066383688caa9b0e308
micra <karagodin.kirill.a@ya.ru>
2023-04-03 15:03:00 +0300

    update helm-stage.sh

--- files/helm-stage.sh
+++ files/helm-stage.sh
@@ -3,4 +3,4 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 kubectl create namespace stage
-helm install node-exporter prometheus-community/prometheus-node-exporter -n prod
+helm install node-exporter prometheus-community/prometheus-node-exporter -n stage
