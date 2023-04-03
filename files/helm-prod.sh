c6c8015844ba393446fe7a86d30343076738b5d9
micra <karagodin.kirill.a@ya.ru>
2023-04-03 14:42:48 +0300

    update prod_site.yml

--- files/helm-prod.sh
+++ files/helm-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+kubectl create namespace prod
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-kubectl create namespace prod
 helm install node-exporter prometheus-community/prometheus-node-exporter -n prod
