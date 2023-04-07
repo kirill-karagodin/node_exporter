2b09b837dd41e88811356360a55ea311e254f631
micra <karagodin.kirill.a@ya.ru>
2023-04-07 18:29:06 +0300

    update helm-stage.sh

--- files/helm-prod.sh
+++ files/helm-prod.sh
@@ -4,3 +4,4 @@ kubectl create namespace prod
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install node-exporter prometheus-community/prometheus-node-exporter -n prod
+helm install kube-state-metrics prometheus-community/kube-state-metrics -n prod
--- files/helm-stage.sh
+++ files/helm-stage.sh
@@ -4,3 +4,4 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 kubectl create namespace stage
 helm install node-exporter prometheus-community/prometheus-node-exporter -n stage
+helm install kube-state-metrics prometheus-community/kube-state-metrics -n stage
